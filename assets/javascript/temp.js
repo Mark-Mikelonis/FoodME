1499081ddb53284201b5f8a4671659e02452b920
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-02-04 16:34:59 -0800

    adding things

---
+++ assets/javascript/temp.js
@@ -0,0 +1,6 @@
+$("#makeit-img").mouseover( function(){
+   $(this).attr("src", "assets/images/makeit_btn2.png");
+});
+$("#makeit-img").mouseout( function(){
+   $(this).attr("src", "assets/images/makeit-btn.png");
+});
