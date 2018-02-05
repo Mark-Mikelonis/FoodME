7979a257966d9178019d6b0f1f120883477f2015
psharif <psharif@asu.edu>
2018-02-04 19:10:54 -0800

    Added deliver it form, and action. Using different forms depending on which button is selected. Need to determine how delivery will display in table, and save information with session storage to display in modal.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,26 @@
 
-var apiKey = "ea22f20d6e490ba43d99d8705330edc7";
-
+var recipeApiKey = "ea22f20d6e490ba43d99d8705330edc7";
+var eatStreetApiKey = "67804766f0ca2e3a";
+ 
 $("#makeit-img").on("click", function(){
+	$("#table-body").empty();
+	$("#deliver-it-form").addClass("hidden-content");
+	$("#make-it-form").removeClass("hidden-content");
+});
+
+$("#deliverit-img").on("click", function(){
+	$("#table-body").empty();
+	$("#make-it-form").addClass("hidden-content");
+	$("#deliver-it-form").removeClass("hidden-content");
+});
+
+$("#make-it-submit-button").on("click", function(){
+
+	$("#make-it-form").addClass("hidden-content");
 
 	$("#table-body").empty();
 	
-	var query = $("#searchTerm").val(); 
+	var query = $("#make-it-query").val(); 
 	var searchUrl = "http://food2fork.com/api/search?key=" + apiKey + "&q=" + query;
 
 	$.ajax({
@@ -13,28 +28,50 @@ $("#makeit-img").on("click", function(){
       method: "GET", 
     }).done(function(response) {
       
-      var responseObject = JSON.parse(response);
+	      var responseObject = JSON.parse(response);
 
-      if (responseObject.recipes.length > 0) {
+	      if (responseObject.recipes.length > 0) {
 
-	    for(var i = 0; i < responseObject.recipes.length; i++){
-	      	if (i >= 10){break;}
-	      	else{
-				var newRow = $("<tr>");
-				var newDiv = $("<div>");
-				newDiv.attr("data-recipe-id", responseObject.recipes[i].recipe_id);
-				newDiv.html('<div class="card"><div class="card-body"><div class="search-display" data-toggle="modal" data-target="#exampleModalCenter" data-recipe-id="' + 
-			  		responseObject.recipes[i].recipe_id + '"><img src="' + responseObject.recipes[i].image_url + '"><br><p>Title:<span class="response-text">' 
-			  		+ responseObject.recipes[i].title +'</span></p><br><br><p>URL: <span class="response-text">'
-			  		+ responseObject.recipes[i].source_url +'</span></p><br></div></div></div>');
-			 	newRow.append(newDiv);
-			 	$("#table-body").append(newRow);
-			}
-	    } 
-	  }
-	  else {
-	  	$("#search-results").text("We did not find any results for that search");
-	  }
+		    for(var i = 0; i < responseObject.recipes.length; i++){
+		      	if (i >= 10){break;}
+		      	else{
+					var newRow = $("<tr>");
+					var newDiv = $("<div>");
+					newDiv.attr("data-recipe-id", responseObject.recipes[i].recipe_id);
+					newDiv.html('<div class="card"><div class="card-body"><div class="search-display" data-toggle="modal" data-target="#exampleModalCenter" data-recipe-id="' + 
+				  		responseObject.recipes[i].recipe_id + '"><img src="' + responseObject.recipes[i].image_url + '"><br><p>Title:<span class="response-text">' 
+				  		+ responseObject.recipes[i].title +'</span></p><br><br><p>URL: <span class="response-text">'
+				  		+ responseObject.recipes[i].source_url +'</span></p><br></div></div></div>');
+				 	newRow.append(newDiv);
+				 	$("#table-body").append(newRow);
+				}
+		    } 
+		  }
+		  else {
+		  	$("#search-results").text("We did not find any results for that search");
+		  }
+	});
+});
+
+$("#deliver-it-submit-button").on("click", function(){
+
+	$("#deliver-it-form").addClass("hidden-content");
+
+	$("#table-body").empty();
+
+	var deliveryQuery = $("#deliver-it-query").val();
+	var zipCode = $("#zip-code-input").val();
+
+	var url = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=delivery&search=' + deliveryQuery + '&street-address=' + zipCode + '&access-token=' + eatStreetApiKey;
+
+	$.ajax({
+      url: url,
+      method: "GET" 
+	}).done(function(response) {
+	     
+	    console.log(response);
+		console.log(response.address.city);
+	});
 });
 
 $(document).on("click", ".search-display", function(){
@@ -58,14 +95,29 @@ $(document).on("click", ".search-display", function(){
 	  		+ responseObject.recipe.title +'</span></p><br><br><p>URL: <span class="response-text">'
 	  		+ responseObject.recipe.source_url +'</span></p><br></div></div></div>');
 
-		responseObject.recipe.ingredients.forEach(function(ingredient){
-			var newItem = $("<p>");
-			newItem.text(ingredient); 
-			newDiv.append(newItem);
-		});
+		var newList = $("<ul>");
 
+		responseObject.recipe.ingredients.forEach(function(ingredient){
+			var newItem = $("<li>");
+			newItem.text(ingredient); 
+			newList.append(newItem);
+		});
+		newDiv.append(newList);
 	 	$("#search-results").append(newDiv);
 	});
 
 });
-});
+
+////Switches one display from another.
+/// If the question area is displayed, hide it and make result area displayed. (And Vice-Versa).
+function switchDisplays(displayed, hidden){
+
+	$(hidden).removeClass("displayed-Content");
+	$(hidden).addClass("hidden-content");
+	//Displayed area becomes unhidden and displays it's content.
+	$(displayed).removeClass("hidden-content");
+	$(displayed).addClass("displayed-content");
+
+}
+
+
