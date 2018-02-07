cf2f508492c5cc7692b293ab5e474e20d2aa1571
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-02-06 16:18:42 -0800

    remove modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,10 +50,7 @@ var geoAllowed = false;
         password: password,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
-
-      // $('#modalLoginForm').modal('hide');
-              $("#login-btn").trigger("reset");
-              // $("#myform")[0].reset();
+        $("#login-btn").trigger("reset");
 
     });
 
@@ -65,13 +62,6 @@ var geoAllowed = false;
       // Console.loging the last user's data
       console.log(sv.username);
       console.log(sv.password);
-
-
-      // Change the HTML to reflect
-      $("#name-display").text(sv.name);
-      $("#email-display").text(sv.email);
-      $("#age-display").text(sv.age);
-      $("#comment-display").text(sv.comment);
 
       // Handle the errors
     }, function(errorObject) {
@@ -92,6 +82,73 @@ var geoAllowed = false;
 // }).catch(function(error) {
 //   // An error happened.
 // });
+       // Google autofill
+// This example displays an address form, using the autocomplete feature
+      // of the Google Places API to help users fill in the information.
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      var placeSearch, autocomplete;
+      var componentForm = {
+        street_number: 'short_name',
+        route: 'long_name',
+        locality: 'long_name',
+        administrative_area_level_1: 'short_name',
+        country: 'long_name',
+        postal_code: 'short_name'
+      };
+
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['geocode']});
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
+      }
+
+      function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var autoplace = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+          document.getElementById(component).value = '';
+          document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the autoplace details place
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < autoplace.address_components.length; i++) {
+          var addressType = autoplace.address_components[i].types[0];
+          if (componentForm[addressType]) {
+            var val = autoplace.address_components[i][componentForm[addressType]];
+            document.getElementById(addressType).value = val;
+          }
+        }
+      }
+
+      // Bias the autocomplete object to the user's geographical location,
+      // as supplied by the browser's 'navigator.geolocation' object.
+      function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      }
 
 
 
@@ -277,30 +334,30 @@ function getDetails() {
 
 
 
-function initMap() {
+// function initMap() {
 
 
-    var service = new google.maps.places.PlacesService($("#table-body").get(0));
-    service.nearbySearch({
-        location: currLoc,
-        radius: 3000,
-        type: ['restaurant'],
-        keyword: searchTerm
-    }, callback);
-}
+//     var service = new google.maps.places.PlacesService($("#table-body").get(0));
+//     service.nearbySearch({
+//         location: currLoc,
+//         radius: 3000,
+//         type: ['restaurant'],
+//         keyword: searchTerm
+//     }, callback);
+// }
 
-function getDetails() {
-    var service = new google.maps.places.PlacesService($("#table-body").get(0));
-    service.getDetails({
-        placeId: placeId
-    }, function(place, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            console.log("in getDetails");
-            // console.log(place);
-            createPlaceList(place);
-        }
-    });
-}
+// function getDetails() {
+//     var service = new google.maps.places.PlacesService($("#table-body").get(0));
+//     service.getDetails({
+//         placeId: placeId
+//     }, function(place, status) {
+//         if (status === google.maps.places.PlacesServiceStatus.OK) {
+//             console.log("in getDetails");
+//             // console.log(place);
+//             createPlaceList(place);
+//         }
+//     });
+// }
 
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
